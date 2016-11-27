15a13b9409b3979fc2ab95541b8e01d9934860ad
ramusz1 <ramusz@wp.pl>
2016-11-27 15:47:51 +0100

    Game Panel basic functions

--- goclient/src/goclient/GamePanel.java
+++ goclient/src/goclient/GamePanel.java
@@ -1,8 +1,54 @@
 package goclient;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+	private final int fieldSize=20;
+	private final int fieldCount=19;				//zrobi żeby dało się zmienia		
+	private int size;
+	private int pawnSize=16;
+	private Color boardColor;
+	private int[][] board= new int[fieldCount][fieldCount];
+	public void makeMove(int x,int y,int color){
+		board[x][y]=color;
+	}
+	public void setBoard(int[][] board){
+		this.board=board;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		g.setColor(boardColor);			
+		g.fillRect(fieldSize, fieldSize, size-fieldSize, size-fieldSize);
+		g.setColor(Color.BLACK);
+		for(int i=1;i<=fieldCount;i++){		//kreski
+			g.drawLine(i*fieldSize, fieldSize, i*fieldSize, size-fieldSize);	//piopnowe
+			g.drawLine(fieldSize, i*fieldSize, size-fieldSize, i*fieldSize);	//poziome
+		}
+		for(int x=0;x<fieldCount;x++){		//pionki
+			for(int y=0;y<fieldCount;y++){
+				if(board[x][y]!=0){
+					if(board[x][y]==1)
+						g.setColor(Color.WHITE);
+					else
+						g.setColor(Color.BLACK);
+					g.fillOval((x+1)*fieldSize, y*fieldSize, pawnSize, pawnSize);
+				}
+			}
+		}
+		
+		
+	}
+	public GamePanel(){
+		size=fieldSize*(fieldCount+1);
+		setPreferredSize(new Dimension(size,size));
+		setSize(size,size);
+		boardColor= new Color(219,178,92);		//Brown
+	}
 	
 	public void waitForMove(Move move) {
 	//TODO wszystko	
@@ -10,4 +56,5 @@ public class GamePanel extends JPanel {
 	public void stopWaitingForMove() {
 		//TODO
 	}
+	
 }
