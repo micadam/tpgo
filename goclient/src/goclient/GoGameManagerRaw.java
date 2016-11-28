e1377b1e99db7e6b87229b05103e58ccc8e24905
ramusz1 <ramusz@wp.pl>
2016-11-28 13:04:44 +0100

    Singleplayer or multiplayer choice

--- goclient/src/goclient/GoGameManagerRaw.java
+++ goclient/src/goclient/GoGameManagerRaw.java
@@ -1,14 +1,23 @@
 package goclient;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
+
 public class GoGameManagerRaw implements GoGameManager {
 	private int gameBoard[][];
 	private int fieldsLeft;
 	private static final int BOARD_SIZE = 19;
 	int currentColor = -1;
+	
 	public GoGameManagerRaw(){
 		gameBoard=new int[BOARD_SIZE][BOARD_SIZE];
 		fieldsLeft=BOARD_SIZE*BOARD_SIZE;
 		currentColor = Move.BLACK_NUMER;
+		AuthWindowRaw awr = new AuthWindowRaw(this);
 	}
 	
 	public int[][] getBoard() {
@@ -24,8 +33,8 @@ public class GoGameManagerRaw implements GoGameManager {
 		for(int x=0;x<BOARD_SIZE && move==null;x++){
 			for(int y=0;y<BOARD_SIZE ;y++){
 				if(gameBoard[x][y]==0){ 
-					move=new Move(x,y, currentColor);
 					makeMove(x, y);
+					move=new Move(x,y, currentColor);
 					break;
 				}
 			}
@@ -59,4 +68,37 @@ public class GoGameManagerRaw implements GoGameManager {
 		currentColor *= -1;
 		return 1;
 	}
+	public void setGameType(String type){
+		System.out.println("Game type set to "+type);
+	}
+}
+class AuthWindowRaw extends JDialog{
+	public AuthWindowRaw(GoGameManagerRaw ggmr){
+		setModal(true);
+		setLocationRelativeTo(null);
+		setLayout(new FlowLayout());
+		Button multiplayer = new Button("Multiplayer");
+		multiplayer.addActionListener(new AuthButtonListener(ggmr,this));
+		Button singleplayer = new Button("Singleplayer");
+		singleplayer.addActionListener(new AuthButtonListener(ggmr,this));
+		add(multiplayer);
+		add(singleplayer);
+		
+		pack();
+		setVisible(true);
+	}
+}
+class AuthButtonListener implements ActionListener{
+	private GoGameManagerRaw goGameManagerRaw;
+	private AuthWindowRaw parent;
+	public AuthButtonListener(GoGameManagerRaw ggmr,AuthWindowRaw parent){
+		this.goGameManagerRaw=ggmr;
+		this.parent=parent;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		goGameManagerRaw.setGameType(e.getActionCommand());
+		parent.dispose();
+	}
+	
 }
