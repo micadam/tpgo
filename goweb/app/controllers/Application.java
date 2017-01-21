8b01234b56e37d0a22c1930aff756413352fdd59
Adam <aadam.michalski@gmail.com>
2017-01-21 18:53:35 +0100

    Further baby steps

--- goweb/app/controllers/Application.java
+++ goweb/app/controllers/Application.java
@@ -1,14 +1,25 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    public static Result joinTable(String tableName) {
+    	
+    	System.out.println(tableName);
+    	
+    	return ok(views.html.gameBoard.render("testt"));
+    	
+    }
+    
+    public static Result gameBoard() {
+    	return ok(views.html.gameBoard.render("test"));
     }
 
 }
